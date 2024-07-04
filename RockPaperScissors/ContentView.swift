9306d8859bbe1f40f0ca72db34d59b6995c66248
bbaneydev <baneyjb@gmail.com>
2024-07-04 11:24:20 -0500

    finished initial game, game works, but repeats questions a lot, going to revise and make it cleaner next

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let allAnswers = ["🪨", "📜", "✂️"]
+    
+    @State private var computerChoice = allAnswers.shuffled()
+    @State private var playerSelection = Bool.random()
+    @State private var computerSelection = Int.random(in: 0...2)
+    @State private var score = 0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Score: \(score)")
+                .font(.largeTitle.bold())
+                .foregroundStyle(.blue)
         }
         .padding()
+        
+        VStack {
+            Text(computerChoice[computerSelection])
+                .font(.system(size: 100))
+            if playerSelection == true {
+                Text("Choose the winning move")
+                    .foregroundStyle(.blue)
+            } else {
+                Text("Choose the losing move")
+                    .foregroundStyle(.red)
+            }
+        }
+        
+        Spacer()
+        
+        HStack {
+            Button {
+                if computerChoice[computerSelection] == "✂️" && playerSelection == true {
+                    score += 1
+                    askQuestion()
+                } else if computerChoice[computerSelection] == "📜" && playerSelection == false {
+                    score += 1
+                    askQuestion()
+                }
+            } label: {
+                Text("Rock")
+            }
+            
+            Button {
+                if computerChoice[computerSelection] == "🪨" && playerSelection == true {
+                    score += 1
+                    askQuestion()
+                } else if computerChoice[computerSelection] == "✂️" && playerSelection == false {
+                    score += 1
+                    askQuestion()
+                }
+            } label: {
+                Text("Paper")
+            }
+            
+            Button {
+                if computerChoice[computerSelection] == "📜" && playerSelection == true {
+                    score += 1
+                    askQuestion()
+                } else if computerChoice[computerSelection] == "🪨" && playerSelection == false {
+                    score += 1
+                    askQuestion()
+                }
+            } label: {
+                Text("Scissors")
+            }
+        }
+        Spacer()
+    }
+    
+    func askQuestion() {
+        computerChoice.shuffle()
+        playerSelection.toggle()
     }
 }
 
